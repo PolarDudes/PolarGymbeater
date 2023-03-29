89cb0b7e36bc49e87f7b9150b983a96bbfe3395d
Andy <pmduck@hotmail.com>
2023-03-29 14:07:23 +0300

    got to the point were it behaves just like axios

--- api/router.js
+++ api/router.js
@@ -1,32 +1,23 @@
-import React, { Component } from 'react'
 import { BASE_URL, ACESS_TOKEN, USER_ID } from '@env';
 
-export default class Router extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            data: [],
-            isLoading: true,
-        }
-    }
+class Router {
 
     async getUser() {
         try {
-            const response = await fetch(`${BASE_URL}/users/${USER_ID}`, {
+            return await fetch(`${BASE_URL}users/${USER_ID}`, {
                 method: 'GET',
                 headers: {
                     Accept: 'application/json',
                     Authorization: `Bearer ${ACESS_TOKEN}`,
-                },
+                }
             })
-            const json = await response.json()
-            this.setState({ data: json })
-            console.log(this.state.data)
-        } catch (error) {
-            console.log(error)
-        } finally {
-            console.log('finally')
+                .then((response) => response.json())
+                .then((json) => json)
+        }
+        catch (error) {
+            return error;
         }
     }
 }
+
+export default new Router;
