3b457113d7b32d93c31e49f565b4dfa66b91d5e6
BuildTools <andy0ph@gmail.com>
2023-03-29 12:18:28 +0300

    trying for fetch

--- api/router.js
+++ api/router.js
@@ -1,38 +1,32 @@
+import React, { Component } from 'react'
 import { BASE_URL, ACESS_TOKEN, USER_ID } from '@env';
-import axiox from 'axios';
 
-// Set autorization header for all requests, could be usefull
-// axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
+export default class Router extends Component {
+    constructor(props) {
+        super(props)
 
-class router {
-    constructor() {
-        this.url = BASE_URL;
-        this.token = ACESS_TOKEN;
-        this.id = USER_ID;
-        this.config = {
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${this.token}`
-            }
+        this.state = {
+            data: [],
+            isLoading: true,
         }
     }
 
-    async get_user_information() {
-        let info = ''
-
-        // return await axiox.get(`${this.url}users/${this.id}`, this.config).then((response) => {
-        //     return response.data
-        // }).catch((error) => {
-        //     console.log(error)
-        // })
-
-        await axiox.get(`${this.url}users/${this.id}`, this.config).then((response) => {
-            info = response.data
-        })
-
-        return info
-
+    async getUser() {
+        try {
+            const response = await fetch(`${BASE_URL}/users/${USER_ID}`, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${ACESS_TOKEN}`,
+                },
+            })
+            const json = await response.json()
+            this.setState({ data: json })
+            console.log(this.state.data)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            console.log('finally')
+        }
     }
 }
-
-export default new router;
