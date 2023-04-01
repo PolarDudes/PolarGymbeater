9a3913f164c85ea171ab79d56e3b95a29f966e5d
BuildTools <andy0ph@gmail.com>
2023-04-01 09:54:13 +0300

    fetches finnish and ready to work

--- api/router.js
+++ api/router.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react'
 import { BASE_URL, ACESS_TOKEN, USER_ID } from '@env';
 
-export default class Router extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            data: [],
-            isLoading: true,
+class Router {
+    constructor() {
+        this.config = {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + ACESS_TOKEN
+            }
         }
     }
 
-    async getUser() {
-        try {
-            const response = await fetch(`${BASE_URL}/users/${USER_ID}`, {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                    Authorization: `Bearer ${ACESS_TOKEN}`,
-                },
-            })
-            const json = await response.json()
-            this.setState({ data: json })
-            console.log(this.state.data)
-        } catch (error) {
-            console.log(error)
-        } finally {
-            console.log('finally')
-        }
+    _fetchData = async (url) => {
+        return new Promise(async (resolve, reject) => {
+            fetch(url, this.config)
+                .then(response => response.json())
+                .then(json => resolve(json))
+                .catch(error => reject(error))
+        })
     }
+
+    getUser() {
+        return this._fetchData(BASE_URL + 'users/' + USER_ID)
+    }
+
+    getExercises() {
+        return this._fetchData(BASE_URL + 'exercises')
+    }
+
 }
+
+export default new Router();
