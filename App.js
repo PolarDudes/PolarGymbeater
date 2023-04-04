eaf2bffac22b1ba974b010966b009d9ce6eb71b3
Andy <pmduck@hotmail.com>
2023-04-04 14:49:47 +0300

    changed from single page with module into multiple page system

--- App.js
+++ App.js
@@ -1,7 +1,47 @@
-import Home from './src/Home';
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+
+import Router from './api/router';
+import Loading from './pages/loading';
 
 export default function App() {
+  const [ userData, setUserData ] = useState([]);
+  const [ exerciseData, setExerciseData ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    let didCancel = false;
+
+    const user = () => Router.getUser()
+      .then(response => setUserData(response))
+
+    const exercises = () => Router.getExercises()
+      .then(response => setExerciseData(response))
+
+    if (!didCancel) {
+      Promise.all([ user(), exercises() ])
+        .catch(e => console.error(e))
+        .finally(() => setLoading(false));
+    }
+
+    return () => didCancel = true;
+  }, [])
+
   return (
-    <Home />
+    <>
+      {loading
+        ?
+        <Loading />
+        : (
+          <View style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+            <Text>App</Text>
+          </View>
+        )}
+    </>
   );
 }
