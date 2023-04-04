1da0e5b21fa26f54b982e2807797e1402a66d255
Andy <pmduck@hotmail.com>
2023-04-04 09:52:40 +0300

    user and exercise data fetched, with example

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Router from '../api/router';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+    const [ userData, setUserData ] = useState([]);
+    const [ exerciseData, setExerciseData ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+        let didCancel = false;
+
+        if (!didCancel) {
+            Router.getUser()
+                .then(response => setUserData(response))
+                .catch(error => console.error(error))
+                .finally(() => { setLoading(false) });
+        }
+
+        return () => didCancel = true;
+    }, [])
+
+    useEffect(() => {
+        let didCancel = false;
+
+        if (!didCancel) {
+            Router.getExercises()
+                .then(response => setExerciseData(response))
+                .catch(error => console.error(error))
+                .finally(() => { setLoading(false) });
+        }
+
+        return () => didCancel = true;
+    }, [])
+
     return (
-        <View style={styles.container}>
-            <Text>Home</Text>
-            <StatusBar style="auto" />
-        </View>
+        <>
+            {loading
+                ?
+                <View style={styles.container}>
+                    <Text >Loading...</Text>
+                </View>
+                : (
+                    <View style={styles.container}>
+                        <Text>Home</Text>
+                        {/* <Esimerrki props={data} /> */}
+                        <StatusBar style="auto" />
+                    </View>
+                )}
+        </>
     );
 }
 
