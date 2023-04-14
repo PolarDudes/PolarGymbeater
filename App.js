c5170e7c7b473f2978e1364f7c964b901ed304a5
Andy <pmduck@hotmail.com>
2023-04-14 13:45:43 +0300

    under construction

--- App.js
+++ App.js
@@ -5,21 +5,24 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Loading from './pages/loading'
 import Home from './pages/home'
+import Enter from './pages/enter'
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <>
-      <PolarContextProvider>
-        <Loading>
-          <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </Loading>
-      </PolarContextProvider>
-    </>
+    // <>
+    //   <PolarContextProvider>
+    //     <Loading>
+    //       <NavigationContainer>
+    //         <Stack.Navigator screenOptions={{ headerShown: false }}>
+    //           <Stack.Screen name="Home" component={Home} />
+    //         </Stack.Navigator>
+    //       </NavigationContainer>
+    //     </Loading>
+    //   </PolarContextProvider>
+    // </>
+
+    <Enter />
   )
 }
