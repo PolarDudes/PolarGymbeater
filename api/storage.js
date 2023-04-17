22fb079989b6df70e876a741c7020804bfce5dda
Andy <60817686+AvaruusAnkka@users.noreply.github.com>
2023-04-17 06:54:21 +0000

    Storage (#8)
    * exercise class added, not needed atm
    
    * added user class
    
    * storage api function ready to be used
    
    * can access multiple storages, induvidually
    
    * npm update
    
    * removed class cause of unusage of them, npm i

---
+++ api/storage.js
@@ -0,0 +1,46 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+class Storage {
+  constructor(key) {
+    this._key = key
+  }
+
+  store = async (value) => {
+    try {
+      AsyncStorage.setItem(this._key, JSON.stringify(value))
+    } catch (e) {
+      console.error(e)
+    }
+    // finally { console.log("Data stored"); }
+  }
+
+  get = async () => {
+    return new Promise(async (resolve, reject) => {
+      AsyncStorage.getItem(this._key)
+        .then((value) => resolve(value != null ? JSON.parse(value) : null))
+        .catch((e) => reject(e))
+    })
+  }
+
+  set = async (value) => {
+    try {
+      AsyncStorage.mergeItem(this._key, JSON.stringify(value))
+    } catch (e) {
+      console.error(e)
+    }
+    // finally { console.log("Data set"); }
+  }
+
+  //TODO: Add function to change values (add user class)
+
+  clear = async () => {
+    try {
+      AsyncStorage.clear()
+    } catch (e) {
+      console.error(e)
+    }
+    // finally { console.log("Data cleared"); }
+  }
+}
+
+export default Storage
