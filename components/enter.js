27e5652948e5478f09771810821cee8a51112dce
Andy <60817686+AvaruusAnkka@users.noreply.github.com>
2023-04-19 10:09:29 +0000

    enter file without changes on others (#15)

---
+++ components/enter.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  Button,
+} from 'react-native'
+import Storage from '../api/storage'
+
+export default function Enter() {
+  const [expanded, setExpanded] = useState(false)
+  const [Food, setFood] = useState('')
+  const [Calories, setCalories] = useState('')
+  const [Data, setData] = useState([])
+
+  const storage = new Storage('@calories')
+
+  React.useEffect(() => {
+    let didCancel = false
+
+    if (!didCancel) {
+      storage
+        .get()
+        .then((data) => {
+          if (data != null) {
+            setData(data)
+          }
+        })
+        .catch((e) => console.error(e))
+    }
+
+    return () => (didCancel = true)
+  }, [])
+
+  const saveData = () => {
+    let currentData = Data
+    currentData.push({ Date: Date.now(), Food: Food, Calories: Calories })
+    storage.store(currentData)
+    setData(currentData)
+    setCalories('')
+    setFood('')
+    setExpanded(false)
+  }
+
+  return (
+    <>
+      {expanded ? (
+        <View style={styles.main}>
+          <TextInput
+            placeholder="What did you eat?"
+            style={styles.input}
+            onChangeText={(text) => setFood(text)}
+            value={Food}
+            autoFocus={true}
+          />
+          <TextInput
+            placeholder="Enter calories"
+            style={styles.input}
+            onChangeText={(text) => setCalories(text)}
+            value={Calories}
+            keyboardType="numeric"
+          />
+          <Button title="Submit" onPress={() => saveData()} />
+        </View>
+      ) : (
+        <TouchableOpacity style={styles.main} onPress={() => setExpanded(true)}>
+          <Text style={{ fontSize: 40 }}>Enter calories</Text>
+        </TouchableOpacity>
+      )}
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EBEEF0',
+    borderRadius: 10,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    fontSize: 20,
+    alignItems: 'center',
+  },
+})
